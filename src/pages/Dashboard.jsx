c88da6c6e3945f4ad1ff223a8ddc6a4ec3cf533a
Jehan <mandaljehaan@gmail.com>
2026-04-02 20:41:44 +0530

    update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setRole } from "../store/financeSlice";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 import { useState, useRef, useEffect } from "react";
 
 export default function Dashboard() {
   const dispatch = useDispatch();
   const role = useSelector((state) => state.finance.role);
+  const location = useLocation();
 
   const linkStyle = ({ isActive }) =>
     `transition ${
@@ -17,7 +18,6 @@ export default function Dashboard() {
   const [open, setOpen] = useState(false);
   const dropdownRef = useRef(null);
 
- 
   useEffect(() => {
     const handleClickOutside = (e) => {
       if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
@@ -35,8 +35,7 @@ export default function Dashboard() {
 
   return (
     <div className="flex flex-col min-h-screen text-gray-300 relative">
-
-      <nav className="sticky top-0 z-999 backdrop-blur-xl bg-white/5 border-b border-white/10 shadow-[0_8px_32px_rgba(0,0,0,0.25)]">
+      <nav className="sticky top-0 z-[999] backdrop-blur-xl bg-white/5 border-b border-white/10 shadow-[0_8px_32px_rgba(0,0,0,0.25)]">
         <div className="w-full flex flex-col md:flex-row justify-between items-center px-6 py-4 gap-4 md:gap-0">
           <h1 className="text-lg font-medium text-gray-200">FinDashUI</h1>
 
@@ -51,38 +50,38 @@ export default function Dashboard() {
               Insights
             </NavLink>
 
-           
-            <div ref={dropdownRef} className="relative">
-              <div
-                onClick={() => setOpen((prev) => !prev)}
-                className="flex items-center justify-between bg-white/5 border border-white/10 rounded-md px-3 py-1 text-gray-300 min-w-20 cursor-pointer"
-              >
-                <span>{roles.find((r) => r.value === role)?.label}</span>
-                <span className="ml-2 text-gray-400">&#9662;</span>
-              </div>
-
-              {open && (
-                <div className="absolute right-0 mt-1 w-full bg-[#111] border border-white/10 rounded-md overflow-hidden z-50">
-                  {roles.map((r) => (
-                    <div
-                      key={r.value}
-                      onClick={() => {
-                        dispatch(setRole(r.value));
-                        setOpen(false); 
-                      }}
-                      className="px-3 py-2 hover:bg-cyan-500/20 cursor-pointer text-gray-200"
-                    >
-                      {r.label}
-                    </div>
-                  ))}
+            {location.pathname === "/transactions" && (
+              <div ref={dropdownRef} className="relative">
+                <div
+                  onClick={() => setOpen((prev) => !prev)}
+                  className="flex items-center justify-between bg-white/5 border border-white/10 rounded-md px-3 py-1 text-gray-300 min-w-20 cursor-pointer"
+                >
+                  <span>{roles.find((r) => r.value === role)?.label}</span>
+                  <span className="ml-2 text-gray-400">&#9662;</span>
                 </div>
-              )}
-            </div>
+
+                {open && (
+                  <div className="absolute right-0 mt-1 w-full bg-[#111] border border-white/10 rounded-md overflow-hidden z-50">
+                    {roles.map((r) => (
+                      <div
+                        key={r.value}
+                        onClick={() => {
+                          dispatch(setRole(r.value));
+                          setOpen(false);
+                        }}
+                        className="px-3 py-2 hover:bg-cyan-500/20 cursor-pointer text-gray-200"
+                      >
+                        {r.label}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </nav>
 
-     
       <main className="flex-1 relative z-10 px-6 py-10">
         <Outlet />
       </main>
